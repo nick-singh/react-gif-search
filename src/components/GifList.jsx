a3379b5f80cfca726db913b6cbf85efa8dde3186
Nicholas Chamansingh <nicholaschamansingh@Nicholass-MacBook-Pro.local>
2017-09-06 14:23:02 -0400

    completed searchbar with gif list

--- src/components/GifList.jsx
+++ src/components/GifList.jsx
@@ -3,8 +3,8 @@ import GifItem from './GifItem';
 
 
 const GifList = (props) => {
-  
   const gifItems = props.gifs.map((image) => {
+
     return <GifItem key={image.id}
                     gif={image}
                     onGifSelect={props.onGifSelect} />
