5053adccb80da420085ca2695efce4a5550ca79c
Nicholas Chamansingh <nicholaschamansingh@Nicholass-MacBook-Pro.local>
2017-09-06 23:13:41 -0400

    added some new containers for routing

---
+++ src/containers/Favourites.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Favourites extends React.Component {
+  render() {
+    return (
+      <div>My Favourites</div>
+    )
+  }
+}
---
+++ src/containers/Signup.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+class SignUp extends React.Component {
+  render(){
+    <div>SignUp</div>
+  }
+}
+
+export default Signup;
